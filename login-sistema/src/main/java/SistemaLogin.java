702991feb7f60efc7c26d8fefb66697464f0bddb
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-09-12 20:33:47 -0300

    Colocando o java em um loop

--- login-sistema/src/main/java/SistemaLogin.java
+++ login-sistema/src/main/java/SistemaLogin.java
@@ -5,19 +5,21 @@ public class SistemaLogin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Autenticacao logar = new Autenticacao();
-
-        System.out.println("LOGIN PLATAFORMA DE MONITORAMENTO");
+        Boolean logou = false;
+        while(!logou){
+            System.out.println("LOGIN PLATAFORMA DE MONITORAMENTO");
         System.out.println("Digite o seu email para efetuar o login");
         String email = scanner.nextLine();
         System.out.println("Digite a sua senha");
         String senha = scanner.nextLine();
 
         Boolean login = logar.autenticar(email, senha);
-
+        logou = login;
         if (login){
             System.out.println("Parabéns, você logou com sucesso");
         } else {
             System.out.println("Email ou senha estão incorretos! Verifique e tente novamente");
+        }
         }
     }
 }
