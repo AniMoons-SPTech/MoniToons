0333e00633040ac258de2bdf6d3bf8ca7c5f0027
AikoJu <julya.aono@sptech.school>
2023-10-26 11:34:56 -0300

    Atualização de script

--- site/public/js/funcionario/listarFuncionario.js
+++ site/public/js/funcionario/listarFuncionario.js
@@ -26,30 +26,31 @@ function listar(){
 
 
     for(var i = 0; i < vetor.length; i++){
-        div_cards.innerHTML += `
-            <div class="card-funcionario">
-                <div class="imagem-funcionario">
-                    <img src="../assets/Lovepik_com-828906606-Hand drawn office computer character scene design element 03 1.png" alt="">
-                    <img class="botao-atualizar-funcionario" src="../assets/att-func.png" onclick="mostrarPopupAtualizarFunc(),atualizar(${vetor[i].idUsuario})">
+        cards_func.innerHTML += `
+        <div class="card-funcionario">
+                    <div class="imagem-funcionario">
+                        <img src="../assets/Lovepik_com-828906606-Hand drawn office computer character scene design element 03 1.png" alt="">
+                        <img class="botao-atualizar-funcionario" src="../assets/att-func.png" onclick="mostrarPopupAtualizarFunc(),atualizar()">
+                    </div>
+                    <div class="info-funcionario">
+                        <div class="nome-funcionario">
+                            <h1 id="nome_func">${vetor[i].nome}</h1>
+                        </div>
+                        <div class="cargo-funcionario">
+                            Cargo: <span id="cargo_func">${vetor[i].cargo}</span>
+                        </div>
+                        <div class="email-funcionario">
+                            Email: <span id="email_func">${vetor[i].email}</span>
+                        </div>
+                        <div class="telefone-funcionario">
+                            Telefone: <span id="tel_func">${vetor[i].telefone}</span>
+                        </div>
+                    </div>
+                    <div class="excluir-funcionario">
+                        <img src="../assets/excluir-funcionario.png" onclick="mostrarPopupExcluirFunc(),excluir(${vetor[i].idUsuario})">
+                    </div>
                 </div>
-                <div class="info-funcionario">
-                    <div class="nome-funcionario">
-                        <h1 id="nome_func">${vetor[i].nome}</h1>
-                    </div>
-                    <div class="cargo-funcionario">
-                        Cargo: <span id="cargo_func">${vetor[i].cargo}</span>
-                    </div>
-                    <div class="email-funcionario">
-                        Email: <span id="email_func">${vetor[i].email}</span>
-                    </div>
-                    <div class="telefone-funcionario">
-                        Telefone: <span id="tel_func">${vetor[i].telefone}</span>
-                    </div>
-                </div>
-                <div class="excluir-funcionario">
-                    <img src="../assets/excluir-funcionario.png" onclick="mostrarPopupExcluirFunc(),excluir(${vetor[i].idUsuario})">
-                </div>
-            </div>
+        
         `
     }
         
--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,104 +1,109 @@
 -- DROP DATABASE animoons;
-CREATE DATABASE IF NOT EXISTS animoons;
+CREATE DATABASE animoons;
 USE animoons;
+
 CREATE TABLE IF NOT EXISTS empresa (
-  idEmpresa INT AUTO_INCREMENT,
+  idEmpresa INT NOT NULL AUTO_INCREMENT,
   nomeFantasia VARCHAR(50),
   cnpj VARCHAR(20),
   telefone VARCHAR(15),
-  PRIMARY KEY (idEmpresa));
+PRIMARY KEY (idEmpresa));
 
-CREATE TABLE IF NOT EXISTS usuario(
-  idUsuario INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS usuario (
+  idUsuario INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(100),
   cargo VARCHAR(20),
-  documento VARCHAR(20),
-  telefone VARCHAR(15),
-  telefone2 VARCHAR(15) NULL DEFAULT NULL,
+  documento VARCHAR(20) ,
+  telefone VARCHAR(15) ,
+  telefone2 VARCHAR(15) ,
   email VARCHAR(100),
   senha VARCHAR(20),
   plano VARCHAR(60),
   fkEmpresa INT,
-  fkResponsável INT,
-  PRIMARY KEY (idUsuario), 
-  CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa)
-    REFERENCES empresa (idEmpresa),
-  CONSTRAINT fkResponsavel FOREIGN KEY (fkResponsável)
-    REFERENCES usuario (idUsuario)
-    );
+  fkResponsável INT ,
+  PRIMARY KEY (idUsuario),
+  CONSTRAINT fkEmpresa
+    FOREIGN KEY (fkEmpresa)
+    REFERENCES empresa(idEmpresa),
+  CONSTRAINT fkResponsavel
+    FOREIGN KEY (fkResponsável)
+    REFERENCES usuario (idUsuario));
 
 CREATE TABLE IF NOT EXISTS componente (
   idComponente INT AUTO_INCREMENT,
   tipo VARCHAR(45),
   numNucleo INT,
-  capacidade DOUBLE,
-  velocidade DOUBLE,
-  nomeComponente VARCHAR(100) NULL,
+  capacidade DOUBLE ,
+  velocidade DOUBLE ,
+  nomeComponente VARCHAR(100) ,
   PRIMARY KEY (idComponente));
 
-CREATE TABLE IF NOT EXISTS registro (
-	idRegistros INT AUTO_INCREMENT,
-	fkComponente INT,
-	uso DOUBLE,
-	velocidadeGravada DOUBLE,
-	velocidadeLeitura DOUBLE,
-	velocidadeCpu DOUBLE,
-	memoriaUso DOUBLE,
-	memoriaDisponivel DOUBLE,
-	dtHora DATETIME,
-	temperatura DOUBLE,
+CREATE TABLE IF NOT EXISTS registro(
+  idRegistros INT NOT NULL AUTO_INCREMENT,
+  fkComponente INT ,
+  uso DOUBLE ,
+  velocidadeGravada DOUBLE ,
+  velocidadeLeitura DOUBLE ,
+  velocidadeCpu DOUBLE ,
+  memoriaUso DOUBLE ,
+  memoriaDisponivel DOUBLE ,
+  dtHora DATETIME ,
+  temperatura DOUBLE ,
   PRIMARY KEY (idRegistros),
-  CONSTRAINT fkRegistroComponente FOREIGN KEY (fkComponente)
-	REFERENCES componente (idComponente)
-);
+  CONSTRAINT fkRegistroComponente
+    FOREIGN KEY (fkComponente)
+    REFERENCES componente (idComponente));
+
+CREATE TABLE IF NOT EXISTS alertas (
+  idalertas INT NOT NULL AUTO_INCREMENT,
+  dt_hora DATETIME ,
+  grauAlerta VARCHAR(45) ,
+  medidaCap VARCHAR(45) ,
+  fkComponente INT NOT NULL,
+  fkRegistro INT NOT NULL,
+  PRIMARY KEY (idalertas),
+  CONSTRAINT fkComponenteAlerta
+    FOREIGN KEY (fkComponente)
+    REFERENCES registro (fkComponente),
+  CONSTRAINT fkRegistroAlerta
+    FOREIGN KEY (fkRegistro)
+    REFERENCES registro (idRegistros));
+
 
 CREATE TABLE IF NOT EXISTS maquina (
-  idMaquina INT AUTO_INCREMENT,
-  fkUsuario INT NOT NULL,
-  fkComponente INT NOT NULL,
+  idMaquina INT NOT NULL AUTO_INCREMENT,
+  fkUsuario INT,
+  fkComponente INT,
   PRIMARY KEY (idMaquina),
-  CONSTRAINT fkUsuario
-    FOREIGN KEY (fkUsuario)
-    REFERENCES usuario (idUsuario),
   CONSTRAINT fkComponente
     FOREIGN KEY (fkComponente)
-    REFERENCES componente (idComponente)
-    );
+    REFERENCES componente (idComponente),
+  CONSTRAINT fkUsuario
+    FOREIGN KEY (fkUsuario)
+    REFERENCES animoons.usuario (idUsuario));
 
-CREATE TABLE IF NOT EXISTS aplicativosAbertos (
-  idAplicativos INT NOT NULL,
-  nomeAplicativo VARCHAR(50) NULL,
-  dtHora DATETIME,
-  usoCpu DOUBLE,
-  usoRam DOUBLE,
-  usoDisco DOUBLE,
-  usoGpu DOUBLE,
-  fkMaquina INT,
-  PRIMARY KEY (idAplicativos),
+
+CREATE TABLE IF NOT EXISTS aplicativosabertos (
+  idPaginas INT AUTO_INCREMENT,
+  pid INT ,
+  nomeAplicativo VARCHAR(200) ,
+  comando VARCHAR(200) ,
+  usoCpu DOUBLE ,
+  usoDisco DOUBLE ,
+  dtHora DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  fkMaquina INT ,
+  PRIMARY KEY (idPaginas),
   CONSTRAINT fkMaquina
     FOREIGN KEY (fkMaquina)
     REFERENCES maquina (idMaquina));
-    
-CREATE TABLE IF NOT EXISTS aplicativosProibidos (
+
+
+CREATE TABLE IF NOT EXISTS aplicativosproibidos (
   idaplicativosProibidos INT NOT NULL,
-  fkUsuario INT NULL,
-  diretorioApp VARCHAR(50) NULL,
+  nomeAplicativoProibido VARCHAR(70) ,
+  diretorioApp VARCHAR(50) ,
+  fkUsuario INT ,
   PRIMARY KEY (idaplicativosProibidos),
   CONSTRAINT fkUsuarioAplicativos
     FOREIGN KEY (fkUsuario)
     REFERENCES usuario (idUsuario));
-
-CREATE TABLE IF NOT EXISTS alertas (
-  idalertas INT AUTO_INCREMENT,
-  dt_hora DATETIME,
-  grauAlerta  VARCHAR(45) NULL,
-  medidaCap VARCHAR(45) NULL,
-  fkRegistro INT NOT NULL,
-  fkComponente INT NOT NULL,
-  PRIMARY KEY (idalertas),
-  CONSTRAINT fkRegistroAlerta FOREIGN KEY (fkRegistro)
-    REFERENCES registro(idRegistros),
-  CONSTRAINT fkComponenteAlerta FOREIGN KEY(fkComponente)
-	REFERENCES registro(fkComponente));
-    
-    SHOW TABLES FROM animoons;
