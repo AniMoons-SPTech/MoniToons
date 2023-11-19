0a1fe9b167c4f12efe9b48b3c7bfe99e235d3769
Gabriella Roman <gabriella.roman@sptech.school>
2023-11-18 22:52:43 -0300

    add modificacoes

--- site/src/models/alertasModel.js
+++ site/src/models/alertasModel.js
@@ -4,13 +4,23 @@ function carregarAlertas(idEmpresa, idUsuario) {
     console.log("ACESSEI O MEDIDAS MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idEmpresa, idUsuario)
 
     var instrucao = `
-    select alerta.idAlerta, componente.tipo as tipoComponente, registro.dataHora, registro.tipo, registro.dadoValor from alerta
-    join registro on idRegistro = fkRegistro
-    join computadorhascomponente on idCompHasComp = fkCompHasComp
-    join componente on idComponente = fkComponente
-    join computador on idComputador = fkComputador
-    join usuario on idUsuario = fkUsuario
-    where usuario.fkEmpresa = ${idEmpresa}`
+    SELECT
+    alerta.idAlerta,
+    componente.tipo as tipoComponente,
+    DATE_FORMAT(registro.dataHora, '%d/%m/%Y %H:%i') as dataHoraFormatada,
+    registro.tipo,
+    registro.dadoValor,
+    alerta.grauAlerta,
+    usuario.nomeUsuario
+FROM
+    alerta
+    JOIN registro ON idRegistro = fkRegistro
+    JOIN computadorhascomponente ON idCompHasComp = fkCompHasComp
+    JOIN componente ON idComponente = fkComponente
+    JOIN computador ON idComputador = fkComputador
+    JOIN usuario ON idUsuario = fkUsuario
+WHERE
+    usuario.fkEmpresa = ${idEmpresa}`
 
     console.log("Executando a instrução SQL: \n" + instrucao)
     return database.executar(instrucao)
