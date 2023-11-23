a8077f6d5b358dfdbd7dbf3ad7bdda6d6805ef81
AikoJu <julya.aono@sptech.school>
2023-11-23 19:34:21 -0300

    Dash

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -20,7 +20,7 @@ function autenticar(email, senha) {
 }
 
 function listar(idBusca){
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idUsuario)
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idBusca)
     var instrucao = `
         SELECT * FROM usuario WHERE fkGestor = ${idBusca} ORDER BY nomeUsuario ;
     `;
