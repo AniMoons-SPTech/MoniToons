5846af98ee3d041e2a59c2d5d2fd5612549133c4
AikoJu <julya.aono@sptech.school>
2023-11-27 19:56:52 -0300

    Dashboard

--- site/public/js/dashboard/barra.js
+++ site/public/js/dashboard/barra.js
@@ -3,9 +3,11 @@ function mudaBarraApp() {
     var liOpcaoGeral = document.getElementById("li_secao_geral")
     var liAppAbertos = document.getElementById("li_app_abertos")
     var liAppProibidos = document.getElementById("li_app_proibidos")
+    var liFuncionario = document.getElementById("li_secao_funcionarios")
 
     if (sessionStorage.CARGO_USUARIO == "COMUM") {
       liOpcaoGeral.style.display = "none";
+      liFuncionario.style.display = "none"
       liAppAbertos.style.display = "block";
       liAppProibidos.style.display = "none";
     } else if (sessionStorage.CARGO_USUARIO == "ADMINISTRADOR") {
