6c6cf3298be85b1128dfeab529b3bc22acd3c895
AikoJu <julya.aono@sptech.school>
2023-11-28 17:47:58 -0300

    Dashboard

--- site/src/models/componentesModel.js
+++ site/src/models/componentesModel.js
@@ -19,7 +19,7 @@ function getDados(fkCompHasComp){
     JOIN computador c ON chc.fkComputador = c.idComputador
     JOIN especificacoesComponente ec ON chc.fkComponente = ec.fkComponente
     JOIN componente comp ON ec.fkComponente = comp.idComponente
-    WHERE chc.fkCompHasComp = ${fkCompHasComp}`
+    WHERE chc.idCompHasComp =${fkCompHasComp}`
                     
     console.log("Executando \n" + instrucao)                
     return database.executar(instrucao);
@@ -28,21 +28,23 @@ function getDados(fkCompHasComp){
     function dadosGrafico(fkCompHasComp){
         console.log("ACESSEI O COMPONENTES MODEL")
     var instrucao = `SELECT
-    r.*,FORMAT(r.dataHora, 'dd/MM/yyyy HH:mm') as dataHoraFormatada,
-    comp.tipo AS tipoComp
+        r.*,
+        comp.tipo AS tipoComp,
+        FORMAT(r.dataHora, 'dd/MM/yyyy HH:mm') AS dataHoraFormatada
 FROM
     registro r
 JOIN
     computadorHasComponente chc ON r.fkCompHasComp = chc.idCompHasComp
 JOIN
     componente comp ON chc.fkComponente = comp.idComponente
-WHERE chc.idCompHasComp = ${fkCompHasComp}
+WHERE
+    r.fkCompHasComp = 57
 ORDER BY
    r.dataHora DESC
 OFFSET
     0 ROWS
 FETCH FIRST
-    20 ROWS ONLY;`
+    100 ROWS ONLY;`
                     
     console.log("Executando \n" + instrucao)                
     return database.executar(instrucao);
