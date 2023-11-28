e9f7bf42e7af1ed13888e6ef54f8a95b91def451
AikoJu <julya.aono@sptech.school>
2023-11-28 00:22:12 -0300

    Dashboard

--- site/src/models/componentesModel.js
+++ site/src/models/componentesModel.js
@@ -13,28 +13,12 @@ function getComponentes(idUsuario){
 
 function getDados(fkCompHasComp){
     console.log("ACESSEI O COMPONENTES MODEL")
-    var instrucao = `SELECT
-    r.*,
-    ec.*,
-    comp.tipo AS tipoComp,
-    (SELECT SUM(CAST(valor AS DECIMAL(18)))
-     FROM especificacoesComponente
-     WHERE tipoEspecificacao IN ('Núcleos Físicos', 'Núcleos Lógicos')) AS nucleos_total
-    FROM
-    registro r
-    JOIN
-    computadorHasComponente chc ON r.fkCompHasComp = chc.idCompHasComp
-    JOIN
-    especificacoesComponente ec ON chc.fkComponente = ec.fkComponente
-    JOIN
-    componente comp ON ec.fkComponente = comp.idComponente
-    WHERE chc.idCompHasComp = ${fkCompHasComp}
-ORDER BY
-   r.dataHora DESC
-OFFSET
-    0 ROWS
-FETCH FIRST
-    200 ROWS ONLY;`
+    var instrucao = `SELECT  comp.*, ec.*, comp.tipo AS tipoComp
+    FROM computadorHasComponente chc
+    JOIN computador c ON chc.fkComputador = c.idComputador
+    JOIN especificacoesComponente ec ON chc.fkComponente = ec.fkComponente
+    JOIN componente comp ON ec.fkComponente = comp.idComponente
+    WHERE chc.idCompHasComp = ${fkCompHasComp};`
                     
     console.log("Executando \n" + instrucao)                
     return database.executar(instrucao);
