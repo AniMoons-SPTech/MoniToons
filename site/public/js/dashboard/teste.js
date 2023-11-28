9350e62dee95d385f5fb57b59e386a5de24f257e
AikoJu <julya.aono@sptech.school>
2023-11-28 11:15:57 -0300

    Dashboard

--- site/public/js/dashboard/teste.js
+++ site/public/js/dashboard/teste.js
@@ -13,14 +13,8 @@ function plotarGrafico() {
         // Atualize os dados do gráfico existente
         window.myChart.data.labels = dados2.labels;
         window.myChart.data.datasets[0].data = dados2.valores;
-
-        // Atualize opções, se necessário
-        // window.myChart.options = { ... };
-
-        // Atualize o gráfico
         window.myChart.update();
     } else {
-        // Crie um novo gráfico se não existir
         var ctx = document.getElementById('myChart');
         window.myChart = new Chart(ctx, {
             type: 'bar',
@@ -33,20 +27,14 @@ function plotarGrafico() {
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1
                 }]
-            },
-            options: {
-                // Suas opções aqui
             }
         });
     }
 }
 
-plotarGrafico(dados1);
 
-// Agora, suponha que você tem um botão que, quando clicado, chama a função com novos dados
-var botao = document.getElementById('seuBotao');
 
-botao.addEventListener('click', function() {
-    // Chamando a função com novos dados quando o botão é clicado
-    plotarGrafico(dados2);
-});
+
+
+
+
