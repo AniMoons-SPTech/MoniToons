8e69c8e8ab8b54120f80ded73f2fd7abbb490a18
AikoJu <julya.aono@sptech.school>
2023-11-28 11:14:11 -0300

    Dashboard

--- site/public/js/dashboard/teste.js
+++ site/public/js/dashboard/teste.js
@@ -1,9 +1,18 @@
-function plotarGrafico(dados) {
+function plotarGrafico() {
+    var dados1 = {
+        labels: ['Label1', 'Label2', 'Label3'],
+        valores: [10, 20, 30]
+    };
+    
+    var dados2 = {
+        labels: ['LabelA', 'LabelB', 'LabelC'],
+        valores: [40, 50, 60]
+    };
     // Verifique se a variável myChart está definida e é uma instância válida de Chart
     if (typeof window.myChart !== 'undefined' && window.myChart !== null) {
         // Atualize os dados do gráfico existente
-        window.myChart.data.labels = dados.labels;
-        window.myChart.data.datasets[0].data = dados.valores;
+        window.myChart.data.labels = dados2.labels;
+        window.myChart.data.datasets[0].data = dados2.valores;
 
         // Atualize opções, se necessário
         // window.myChart.options = { ... };
@@ -12,14 +21,14 @@ function plotarGrafico(dados) {
         window.myChart.update();
     } else {
         // Crie um novo gráfico se não existir
-        var ctx = document.getElementById('myChart').getContext('2d');
+        var ctx = document.getElementById('myChart');
         window.myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: dados.labels,
+                labels: dados1.labels,
                 datasets: [{
                     label: 'Meu Gráfico',
-                    data: dados.valores,
+                    data: dados1.valores,
                     backgroundColor: 'rgba(75, 192, 192, 0.2)',
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1
@@ -32,18 +41,6 @@ function plotarGrafico(dados) {
     }
 }
 
-// Exemplo de uso:
-var dados1 = {
-    labels: ['Label1', 'Label2', 'Label3'],
-    valores: [10, 20, 30]
-};
-
-var dados2 = {
-    labels: ['LabelA', 'LabelB', 'LabelC'],
-    valores: [40, 50, 60]
-};
-
-// Chamando a função com novos dados
 plotarGrafico(dados1);
 
 // Agora, suponha que você tem um botão que, quando clicado, chama a função com novos dados
