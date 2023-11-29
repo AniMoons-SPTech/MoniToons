0183dad97b75b150dd570f25ad8ec944c8400047
AikoJu <julya.aono@sptech.school>
2023-11-29 13:58:02 -0300

    Dashboard

--- site/public/js/dashboard/dashboard.js
+++ site/public/js/dashboard/dashboard.js
@@ -331,6 +331,7 @@ function atualizarGraficoLinha(fkCompHasComp) {
                 console.log(dadosGrafico);
 
                 console.log(labelDado[0])
+                console.log(novoRegistro[0].dataHora)
                 if (novoRegistro[0].dataHora == labelDado[0]) {
                     console.log("---------------------------------------------------------------")
                     console.log("Como não há dados novos para captura, o gráfico não atualizará.")
