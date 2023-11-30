a17cafe16900bb7fdb54bf63119817b7899f4fdb
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-11-30 02:31:12 -0300

    Colocando os selects dos processo

--- site/src/models/processosModel.js
+++ site/src/models/processosModel.js
@@ -3,7 +3,11 @@ var database = require("../database/config");
 function carregarAplicativosAbertos(idUsuario){
     console.log("ACESSEI O PROCESSOS MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idUsuario)
 
-    var instrucao = ``;
+    var instrucao = `SELECT appsAbertos.pid, appsAbertos.nomeAplicativo, appsAbertos.usoCpu, appsAbertos.usoDisco 
+                    FROM appsAbertos
+                    JOIN computador c ON c.idComputador = appsAbertos.fkComputadorApp
+                    JOIN usuario u ON u.idUsuario = c.fkUsuario
+                    WHERE u.idUsuario = ${idUsuario} `;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -12,25 +16,26 @@ function carregarAplicativosAbertos(idUsuario){
 function carregarAplicativosProibidos(idResponsavel){
     console.log("ACESSEI O PROCESSOS MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idResponsavel)
 
-    var instrucao = ``;
+    var instrucao = `select idAplicativosProibidos, nomeAplicativoProibido from appsProibidos
+                        where fkUsuario = ${idResponsavel}`;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
-function adicionarAplicativoProibido(idResponsavel){
+function adicionarAplicativoProibido(idResponsavel, nomeAplicativo){
     console.log("ACESSEI O PROCESSOS MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idResponsavel)
 
-    var instrucao = ``;
+    var instrucao = `INSERT INTO appsProibidos (nomeAplicativoProibido, fkUsuario) VALUES ('${nomeAplicativo}', ${idResponsavel})`;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
-function atualizarAplicativoProibido(idProcesso){
+function atualizarAplicativoProibido(idProcesso, nomeAplicativo){
     console.log("ACESSEI O PROCESSOS MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idResponsavel)
 
-    var instrucao = ``;
+    var instrucao = `UPDATE appsProibidos SET nomeAplicativoProibido = '${nomeAplicativo}' WHERE idAplicativosProibidos = ${idProcesso}`;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -39,7 +44,7 @@ function atualizarAplicativoProibido(idProcesso){
 function  excluirAppProibido(idProcesso){
     console.log("ACESSEI O PROCESSOS MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar(): ", idResponsavel)
 
-    var instrucao = ``;
+    var instrucao = `DELETE FROM appsProibidos WHERE idAplicativosProibidos = ${idProcesso}`;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
