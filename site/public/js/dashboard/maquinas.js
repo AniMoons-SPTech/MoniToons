d6f270238554a1f9d20024054ea367af118ce02b
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-12-01 13:10:23 -0300

    Colocando set time out nas funções das máquinas

--- site/public/js/dashboard/maquinas.js
+++ site/public/js/dashboard/maquinas.js
@@ -10,6 +10,7 @@ function carregarGrupoMaquinas() {
         if(response.ok) {
             response.json().then((resposta) => {
                 plotarCardsMaquinas(resposta)
+
             })  
         }
     })
@@ -32,43 +33,47 @@ function plotarCardsMaquinas(maquinas) {
         var conteudo = document.createElement("div")
         conteudo.classList.add("conteudo")
 
+
         var spanNomeUser = document.createElement("span")
         spanNomeUser.classList.add("titulo-conteudo")
         spanNomeUser.innerHTML = maquina.nomeUsuario;
 
         var centroConteudo = document.createElement("div")
         centroConteudo.classList.add("centro-conteudo")
+        centroConteudo.setAttribute("id", `statusCardsUsuario${idUser}`)
 
-        for (let i = 0; i < 4; i++) {
+        for (let i = 1; i < 5; i++) {
             var colunaStatus = document.createElement("div")
             colunaStatus.classList.add("coluna_status")
 
             var componente = document.createElement("p")
             switch (i){
-                case 0:
+                case 1:
                     componente.innerHTML = "CPU"
                     break;
-                case 1:
+                case 2:
                     componente.innerHTML = "RAM"
                     break;
-                case 2:
+                case 3:
                     componente.innerHTML = "DISCO" 
                     break;
-                case 3:
+                case 4:
                     componente.innerHTML = "GPU"
                     break;
             }
 
             var iconeStatus = document.createElement("img")
+            iconeStatus.setAttribute("id", `iconeStatus${idUser}${i}`)
             var status = document.createElement("p")
+            status.setAttribute("id", `status${idUser}${i}`)
             var estado = "";
-            if(i == 0){
+            if(i == 1){
                 estado = maquina.statusCpu 
-            } else if (i == 1) {
-                estado = maquina.statusRam
             } else if (i == 2) {
-                estado = maquina.statusDisco
+                estado = maquina.statusRam
             } else if (i == 3) {
+                estado = maquina.statusDisco
+            } else if (i == 4) {
                 estado = maquina.statusGpu
             }
             switch (estado){
@@ -109,6 +114,62 @@ function plotarCardsMaquinas(maquinas) {
         card.appendChild(conteudo);
         cards.appendChild(card); 
     });
+    setInterval(plotarStatusDeTemposEmTempos(), 15000)
+}
+
+
+function plotarStatusDeTemposEmTempos() {
+    fetch(`/maquinas/carregarGrupoMaquinas/${sessionStorage.ID_USUARIO}`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((response) => {
+        if(response.ok) {
+            response.json().then((maquinas) => {
+                maquinas.forEach(maquina => {
+
+                    // ESTRUTURA DA TUPLA
+                    // ID, NOME, STATUS-CPU, STATUS-RAM, STATUS-DISCO, STATUS-GPU
+            
+                    var idUser = maquina.idUser;
+            
+                    for (let i = 1; i < 5; i++) {
+                        var iconeStatus = document.getElementById(`iconeStatus${idUser}${i}`)
+                        var status = document.getElementById(`status${idUser}${i}`)
+            
+                        var estado = "";
+                        if(i == 1){
+                            estado = maquina.statusCpu 
+                        } else if (i == 2) {
+                            estado = maquina.statusRam
+                        } else if (i == 3) {
+                            estado = maquina.statusDisco
+                        } else if (i == 4) {
+                            estado = maquina.statusGpu
+                        }
+                        switch (estado){
+                            case "CRITICO":
+                                iconeStatus.setAttribute("src", "../assets/fogo.png")
+                                status.innerHTML = "Crítico"
+                                break;
+                            case "INTERMEDIARIO":
+                                iconeStatus.setAttribute("src", "../assets/avisoLaranja.png")
+                                status.innerHTML = "Intermediário"
+                                break;
+                            case "MODERADO":
+                                iconeStatus.setAttribute("src", "../assets/avisoAmarelo.png")
+                                status.innerHTML = "Moderado"
+                                break;
+                            default:
+                                iconeStatus.setAttribute("src", "../assets/check.png")
+                                status.innerHTML = "Saudável"
+                        }
+                    }
+                });
+            })  
+        }
+    })    
 }
 
 function abrirDashboard(idUser){
