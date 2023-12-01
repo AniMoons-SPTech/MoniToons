87d48fb8beb12e5385dee2287ca259b09cd3646a
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-12-01 14:16:58 -0300

    Mudando metodos

--- site/src/routes/processos.js
+++ site/src/routes/processos.js
@@ -11,15 +11,15 @@ router.get("/carregarAplicativosProibidos/:idResponsavel", function (req , res) 
     processosController.carregarAplicativosProibidos(req,res);
 })
 
-router.get("/adicionarAplicativoProibido", function (req , res) {
+router.post("/adicionarAplicativoProibido", function (req , res) {
     processosController.adicionarAplicativoProibido(req,res);
 })
 
-router.get("/atualizarAplicativoProibido/:idProcesso", function (req , res) {
+router.post("/atualizarAplicativoProibido/:idProcesso", function (req , res) {
     processosController.atualizarAplicativoProibido(req,res);
 })
 
-router.get("/excluirAppProibido/:idProcesso", function (req , res) {
+router.delete("/excluirAppProibido/:idProcesso", function (req , res) {
     processosController.excluirAppProibido(req,res);
 })
 
